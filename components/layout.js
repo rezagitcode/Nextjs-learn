294c04d139ced69ddde0bf84c2fc8cd22b82eb8c
Reza <mahmudreza1999@gmail.com>
2023-06-08 17:27:44 +0530

    modified index.js

--- components/layout.js
+++ components/layout.js
@@ -31,7 +31,7 @@ export default function Layout({ children, home }) {
             <Image
               priority
               src="/images/profile.jpg"
-              className={utilStyles.borderCircle}
+              className={`${utilStyles.borderCircle} `}
               height={144}
               width={144}
               alt=""
--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,24 @@ import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
-export default function Home() {
+import { getSortedPostsData } from "../lib/post";
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+export default function Home({allPostsData}) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p className="">I am a web app developer</p>
+        <p className="text-blue-500">I am a web app developer</p>
         <Link href="/posts/first-post">
           <h1>This is my First post</h1>
         </Link>
@@ -17,9 +27,20 @@ export default function Home() {
           (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
-        <button className="bg-sky-700 px-4 py-2 text-blue-500 hover:bg-sky-800 sm:px-8 sm:py-3">
-          Reza
-        </button>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
